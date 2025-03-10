44fe22e0141987ee9b2343444e18ab929dc1c8b9
Eduardo <eduardoescobar7@gmail.com>
2025-03-09 22:05:35 -0500

    wip

--- add_llm.py
+++ add_llm.py
@@ -1,22 +1,35 @@
+import dotenv
 import os
 
+import google.generativeai as genai
 from fastrtc import ReplyOnPause, Stream, get_stt_model, get_tts_model
-# from openai import OpenAI
+
+# Load env variables
+dotenv.load_dotenv(".env.local")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
+
 
 sst_model = get_stt_model()
 tts_model = get_tts_model()
+
+chat = model.start_chat()
 
 
 def echo(audio):
     prompt = sst_model.stt(audio)
     # Here you can call your LLM
-    response = f"""
-You asked this: {prompt}
-My answer is: who cares!
-"""
+    response = chat.send_message(prompt)
+    response = response.text
+
+    # Remove asterisks from the response
+    response = response.replace("*", "")
+
     for audio_chunk in tts_model.stream_tts_sync(response):
         yield audio_chunk
 
 
 stream = Stream(ReplyOnPause(echo), modality="audio", mode="send-receive")
-stream.ui.launch(share=True)
+stream.ui.launch()
